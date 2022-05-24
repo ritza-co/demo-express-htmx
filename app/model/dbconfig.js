32adee524173f23542b139fb84b9d28ff0dcf63b
AnesuMurakata <murac96@gmail.com>
2022-05-24 16:45:39 +0200

    Added persistent storage dir

--- app/model/dbconfig.js
+++ app/model/dbconfig.js
@@ -3,7 +3,7 @@ const persistent_path = process.env.PERSISTENT_STORAGE_DIR
  
 const sequelize = new Sequelize("test-db", "user", "pass", {
   dialect: "sqlite",
-  host: "./dev.sqlite"
+  host: persistent_path + "/dev.sqlite"
 });
  
 module.exports = sequelize;
